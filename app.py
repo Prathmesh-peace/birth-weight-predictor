e5d04470852e0e0d31a0ee0589e19af3921f18ba
Prathmesh <prathmeshking45@gmail.com>
2026-03-12 23:07:39 +0530

    my first project

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, render_template
+import pandas as pd
+import pickle
+
+app = Flask(__name__)
+
+def get_cleaned_data(form_data):
+    gestation = float(form_data['gestation'])
+    parity = int(form_data['parity'])
+    age = float(form_data['age'])
+    height = float(form_data['height'])
+    weight = float(form_data['weight'])
+    smoke = float(form_data['smoke'])
+
+    cleaned_data = {"gestation":[gestation],
+                    "parity":[parity],
+                    "age":[age],
+                    "height":[height],
+                    "weight":[weight],
+                    "smoke":[smoke]
+                    }
+    
+    return cleaned_data
+
+@app.route('/', methods = ['GET'])
+def home():
+    return render_template("index.html")
+
+@app.route('/predict', methods = ['POST'])
+def baby_prediction():
+    baby_data = request.form
+    baby_data_cleaned = get_cleaned_data(baby_data)
+    baby_df = pd.DataFrame(baby_data_cleaned)
+    with open("model/model.pkl", "rb") as obj:
+        mymodel = pickle.load(obj)
+    prediction = mymodel.predict(baby_df)
+    prediction = round(float(prediction[0]), 2)
+    response = {"prediction" : prediction}
+    return render_template("index.html", prediction = prediction)
+
+if __name__ == "__main__":
+    app.run()
